1d743b97a60aea477d8bf479cdc33fa1cd9d2910
Michal Lewandowski <michalewy00@gmail.com>
2023-11-05 00:37:12 +0100

    Dodanie pierwszych warunków i zmiennych

---
+++ Kalkulator.py
@@ -0,0 +1,20 @@
+
+print('Cześć i czołem, masz przed sobą najprostrzy kalkualtor na świecie.')
+while True:
+    what_calculation = input("Typy obliczeń: \n 1.Dodawanie \n 2.Odejmowanie \n 3.Mnożenie \n 4.Dzielenie \n Wybierz proszę typ działania, który w tej chwili potrzebujesz:")
+    if what_calculation in ('1','2','3','4'):
+        break
+    print("Błedna cyfra, wybierz cyfrę od 1 do 4")
+if what_calculation == '1':
+    what_numbers = input("Podaj proszę dwie liczby, które chcesz policzyć:")
+    pass
+elif what_calculation == '2':
+    what_numbers = input("Podaj proszę dwie liczby, które chcesz policzyć:")
+    pass
+elif what_calculation == '3':
+    what_numbers = input("Podaj proszę dwie liczby, które chcesz policzyć:")
+    pass
+elif what_calculation == '4':
+    what_numbers = input("Podaj proszę dwie liczby, które chcesz policzyć:")
+    pass
+
