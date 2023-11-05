16783202805e568eb4e7ed6eace961f07bb43285
Michal Lewandowski <michalewy00@gmail.com>
2023-11-05 02:08:14 +0100

    Dodanie funkcji mnożenia i dzielenia

--- Kalkulator.py
+++ Kalkulator.py
@@ -7,24 +7,34 @@ while True:
     print("Błedna cyfra, wybierz cyfrę od 1 do 4")
 if what_calculation == '1':
     How_many_numbers = int(input("Podaj ilość liczb, które chcesz uzyć w obliczeniach:"))
-    what_numbers = input(f"Podaj proszę {How_many_numbers} liczb/y, które chcesz dodać. Pamiętaj aby odizelić je przecinkiem:")
+    what_numbers = input(f"Podaj proszę {How_many_numbers} liczb/y, które chcesz dodać. Pamiętaj aby odzielić je przecinkiem:")
     Split_numbers = what_numbers.split(",")
     suma = 0
     for i in range(How_many_numbers):
         number = int(Split_numbers[i])
         suma += number
-    print(suma)
+    print(f"Twój wynik to {suma}")
 elif what_calculation == '2':
-    what_numbers = input("Podaj proszę dwie liczby, które chcesz odjąć. Pamiętaj aby odizelić je przecinkiem:")
+    what_numbers = input("Podaj proszę dwie liczby, które chcesz odjąć. Pamiętaj aby odzielić je przecinkiem:")
     Split_numbers = what_numbers.split(",")
     First_number = float(Split_numbers[0])
     Second_number = float(Split_numbers[1])
     calculation = First_number - Second_number
-    print(calculation)
+    print(f"Twój wynik to {calculation}")
 elif what_calculation == '3':
-    what_numbers = input("Podaj proszę dwie liczby, które chcesz pomnożyć. Pamiętaj aby odizelić je przecinkiem:")
-    pass
+    How_many_numbers = int(input("Podaj ilość liczb, które chcesz uzyć w obliczeniach:"))
+    what_numbers = input(f"Podaj proszę {How_many_numbers} liczb/y, które chcesz pomnożyć. Pamiętaj aby odzielić je przecinkiem:")
+    Split_numbers = what_numbers.split(",")
+    suma = 1
+    for i in range(How_many_numbers):
+        number = int(Split_numbers[i])
+        suma *= number 
+    print(f"Twój wynik to {suma}")
 elif what_calculation == '4':
-    what_numbers = input("Podaj proszę dwie liczby, które chcesz podzielić. Pamiętaj aby odizelić je przecinkiem:")
-    pass
+    what_numbers = input("Podaj proszę dwie liczby, które chcesz podzielić. Pamiętaj aby odzielić je przecinkiem:")
+    Split_numbers = what_numbers.split(",")
+    First_number = float(Split_numbers[0])
+    Second_number = float(Split_numbers[1])
+    calculation = First_number / Second_number
+    print(f"Twój wynik to {calculation}")
 
